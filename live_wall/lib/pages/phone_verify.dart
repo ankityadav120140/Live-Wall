b000396a13e97dcc29e53c529440f076eb43b0a9
devendrakumargupt <devendrkumar02july2001@gmail.com>
2021-09-30 11:58:20 +0530

    first commit

---
+++ live_wall/lib/pages/phone_verify.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class phone_verify extends StatefulWidget {
+  @override
+  _phone_verifyState createState() => _phone_verifyState();
+}
+
+class _phone_verifyState extends State<phone_verify> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
